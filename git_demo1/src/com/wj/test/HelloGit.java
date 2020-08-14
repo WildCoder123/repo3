5f7af0bbbac3a601300fe01031cd80f3a3d5954c
WildCoder123 <642864386@qq.com>
2020-08-14 13:45:37 +0800

    把本地工程添加到git

---
+++ git_demo1/src/com/wj/test/HelloGit.java
@@ -0,0 +1,15 @@
+package com.wj.test;
+
+/**
+ * @author 王杰
+ * @date 2020/8/14 - 13:40
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git~~~");
+        System.out.println("hello git~~~");
+        System.out.println("hello git~~~");
+        System.out.println("hello git~~~");
+        System.out.println("hello git~~~");
+    }
+}
